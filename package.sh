6b91aad232758af5c7a90c49026f9e15ae5a8d5d
Italo de Rezende <italoeji@hotmail.com>
2026-03-29 15:08:20 -0300

    feat: setup github actions build and secrets

--- package.sh
+++ package.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-rm -rf build/
-zip release clues.json dofhunt.exe
-mkdir -p build
-mv release.zip build
+set -e
+
+ARCH="${1:-arm64}"  # arm64 (Apple Silicon) ou amd64 (Intel)
+OUT="dist/dofushunt-mac-$ARCH"
+
+rm -rf dist/
+mkdir -p "$OUT"
+
+echo "Compilando para darwin/$ARCH..."
+GOOS=darwin GOARCH="$ARCH" go build -ldflags "-s -w" -o "$OUT/dofhunt" .
+
+echo "Copiando arquivos..."
+cp .env "$OUT/.env"
+
+echo "Zipando..."
+cd dist
+zip -r "dofushunt-mac-$ARCH.zip" "dofushunt-mac-$ARCH/"
+cd ..
+
+echo ""
+echo "Pronto! dist/dofushunt-mac-$ARCH.zip gerado."
+echo "Seu amigo precisa ter o Tesseract instalado: brew install tesseract tesseract-lang"
